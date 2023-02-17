aa2cc4684b2df46c2457bcfb7b3ecc184c9e26e3
yohan <dygks34@gmail.com>
2023-02-18 04:00:03 +0900

    feat : data rest 설정

--- src/main/java/com/example/dutchpay/service/FriendRepository.java
+++ src/main/java/com/example/dutchpay/service/FriendRepository.java
@@ -2,6 +2,8 @@ package com.example.dutchpay.service;
 
 import com.example.dutchpay.domain.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 }
--- src/main/java/com/example/dutchpay/service/UserAccountRepository.java
+++ src/main/java/com/example/dutchpay/service/UserAccountRepository.java
@@ -2,6 +2,8 @@ package com.example.dutchpay.service;
 
 import com.example.dutchpay.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long>{
 }
