6cab48e460277efe08680a9a0d91ad710047791f
yohan <dygks34@gmail.com>
2023-02-25 14:50:37 +0900

    feat : 친구 컨트롤러 구현

---
+++ src/main/java/com/example/dutchpay/cotroller/FriendController.java
@@ -0,0 +1,66 @@
+package com.example.dutchpay.cotroller;
+
+import com.example.dutchpay.domain.Friend;
+import com.example.dutchpay.dto.FriendSaveDto;
+import com.example.dutchpay.dto.FriendSelectSaveDto;
+import com.example.dutchpay.service.FriendRepository;
+import com.example.dutchpay.service.FriendService;
+import com.example.dutchpay.service.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.dutchpay.service.FriendService.*;
+
+@Controller
+public class FriendController {
+    private final FriendRepository friendRepository;
+    private final FriendService friendService;
+    private final UserAccountRepository userAccountRepository;
+
+    @Autowired
+    public FriendController(FriendRepository friendRepository,
+                            FriendService friendService,
+                            UserAccountRepository userAccountRepository) {
+        this.friendRepository = friendRepository;
+        this.friendService = friendService;
+        this.userAccountRepository = userAccountRepository;
+    }
+
+    @GetMapping("/friend")
+    public String friendList(Model model) {
+        // todo: 추후에, userAccount.getId() 변경
+        model.addAttribute("friendList", friendRepository.findAllByUserAccountId(1L));
+        model.addAttribute("newFriend", new FriendSaveDto());
+        return "friend";
+    }
+
+    @PostMapping("/friend")
+    public String dutchAddFriend(@ModelAttribute("newFriend") FriendSaveDto friendSaveDto) {
+        friendService.addFriend(friendSaveDto.toEntity(userAccountRepository.findById(1L).orElse(null)));
+        return "redirect:/" + "friend";
+    }
+
+    @GetMapping("/friend/{id}")
+    public String dutchDeleteFriend(@PathVariable Long id) {
+        friendService.deleteFriend(id);
+        return "redirect:/" + "friend";
+    }
+    
+    @GetMapping("/dutch/friend")
+    public String dutchFriendSelect(Model model) {
+        friendService.addFriendSelectListBefore(friendRepository.findAllByUserAccountId(1L));
+        model.addAttribute("friendList", friendSelectListBefore);
+        return "friendSelect";
+    }
+
+    @PostMapping("/dutch/friend")
+    public String dutchFriendSelectSave(@RequestParam("selectedfreind") List<Integer> selectedfreind) {
+        friendService.addFriendSelectListAfter(selectedfreind);
+        return "redirect:/" + "dutch/dutchPayList";
+    }
+}
