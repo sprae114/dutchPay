334a5218c2e5c2035f1b1b999839a22428098f10
yohan <dygks34@gmail.com>
2023-02-25 14:57:31 +0900

    feat : 친구 서비스 구현

---
+++ src/main/java/com/example/dutchpay/service/FriendService.java
@@ -0,0 +1,52 @@
+package com.example.dutchpay.service;
+
+import com.example.dutchpay.domain.Friend;
+import com.example.dutchpay.dto.FriendSelectSaveDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FriendService {
+    public static ArrayList<FriendSelectSaveDto> friendSelectListBefore;
+    public static ArrayList<FriendSelectSaveDto> friendSelectListAfter;
+    public FriendRepository friendRepository;
+
+    @Autowired
+    public FriendService(FriendRepository friendRepository) {
+        this.friendRepository = friendRepository;
+        friendSelectListBefore = new ArrayList<>();
+        friendSelectListAfter = new ArrayList<>();
+    }
+
+    public void addFriend(Friend friend) {
+        friendRepository.save(friend);
+    }
+
+    public void deleteFriend(Long id) {
+        friendRepository.deleteById(id);
+    }
+
+    public void addFriendSelectListBefore(List<Friend> friendList) {
+        initFriendSelectListBefore();
+
+        for (Friend friend : friendList) {
+            friendSelectListBefore.add(friend.toFriendSelectSaveDto(friend));
+        }
+    }
+
+    public void addFriendSelectListAfter(List<Integer> selectedfreind) {
+        initFriendSelectListAfter();
+        selectedfreind.forEach(i -> friendSelectListAfter.add(friendSelectListBefore.get(i)));
+    }
+
+    private void initFriendSelectListBefore() {
+        friendSelectListBefore.clear();
+    }
+
+    private void initFriendSelectListAfter() {
+        friendSelectListAfter.clear();
+    }
+}
