8b1c29902e49c057449e0c1ab85860fe4da0ac84
yohan <dygks34@gmail.com>
2023-02-25 14:54:40 +0900

    fix : friend UserAccountId로 찾는 SQL 추가

--- src/main/java/com/example/dutchpay/service/FriendRepository.java
+++ src/main/java/com/example/dutchpay/service/FriendRepository.java
@@ -4,6 +4,9 @@ import com.example.dutchpay.domain.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+    List<Friend> findAllByUserAccountId(Long userAccountId);
 }
