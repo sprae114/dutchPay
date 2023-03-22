7668a1d4d7e5e1f362f423c626ccc9422af109e1
Yohan <sprae114@naver.com>
2023-03-22 22:37:02 +0900

    feat : DutchResult 구현 및 중복이름으로 인한 서비스이름 수정

--- src/main/java/com/example/dutchpay/service/DutchResultRepository.java
+++ src/main/java/com/example/dutchpay/service/DutchResultRepository.java
@@ -1,9 +1,13 @@
 package com.example.dutchpay.service;
 
 import com.example.dutchpay.domain.DutchResult;
+import com.example.dutchpay.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface DutchResultRepository extends JpaRepository<DutchResult, Long> {
+    List<DutchResult> findAllByUserAccountId(Long userAccountId);
 }
