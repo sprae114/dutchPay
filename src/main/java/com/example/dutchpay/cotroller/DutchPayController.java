c1f092c2785701d700c24bce0246f113ef77c03a
Yohan <sprae114@naver.com>
2023-04-03 01:01:36 +0900

    fix : 로그인 했을때, 메인페이지에 사용자 이름 보이게 수정

--- src/main/java/com/example/dutchpay/cotroller/DutchPayController.java
+++ src/main/java/com/example/dutchpay/cotroller/DutchPayController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.example.dutchpay.service.DutchPayService.dutchpayTotal;
@@ -34,7 +35,11 @@ public class DutchPayController {
     public static List<Long> dutchpayMain = new ArrayList<>();
 
     @GetMapping
-    public String dutchHome() {
+    public String dutchHome(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        Map<String, Object> properties = (Map<String, Object>) principal.getAttribute("properties");
+        String nickname = (String) properties.get("nickname");
+        model.addAttribute("name", nickname);
+
         return "dutchHome";
     }
 
