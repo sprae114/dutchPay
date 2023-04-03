9c371bedf68959bd12eb997b05b93d8c003aec91
Yohan <sprae114@naver.com>
2023-04-03 16:24:29 +0900

    feat : 친구를 선택하지 않았을 때, 오류 상세하게 구현

---
+++ src/main/java/com/example/dutchpay/exception/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.dutchpay.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException ex, Model model) {
+
+        log.error("Missing parameter: " + ex.getParameterName());
+        model.addAttribute("error", "친구를 선택하지 않아 오류가 발생했습니다.");
+
+        return "error/error";
+    }
+}
+
