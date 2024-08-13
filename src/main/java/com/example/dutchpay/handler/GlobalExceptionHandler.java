f141ec607f95ad54af9bc3dd1a24852e4709ccd5
yohan <dygks34@gmail.com>
2024-08-13 23:15:52 +0900

    fix : exception 핸들러 통합

--- src/main/java/com/example/dutchpay/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/dutchpay/handler/GlobalExceptionHandler.java
@@ -1,12 +1,22 @@
 package com.example.dutchpay.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+
+        return "error/4xx";
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
